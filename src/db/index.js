718ab554d545be58a08dc1a05baffdddf8505d29
Manoj Regmi <manojregmi@Manojs-MacBook-Air.local>
2025-11-02 00:35:44 +0530

    db connection

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
+        console.log(`\n Connection Instance: ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.error("MONGODB connection error: ", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
